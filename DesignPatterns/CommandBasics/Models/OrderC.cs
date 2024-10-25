f49a3657437cc96ea0ab6d99ac76f08d7163227a
qasas145 <mohamed.sayed14527@yahoo.com>
2024-10-25 17:47:13 +0300

    Refactor add product method

--- DesignPatterns/CommandBasics/Models/OrderC.cs
+++ DesignPatterns/CommandBasics/Models/OrderC.cs
@@ -1,11 +1,17 @@
+
 public class OrderC : Order{
     public void AddOrder(ProductC product) { 
-        this.OrderLines = new List<OrderLine>() {
-            new OrderLine(){ProductId = product.Id, UnitPrice = product.UnitPrice}
-        };
+        this.OrderLines.Add(
+            new OrderLine(){ProductId = product.Id, UnitPrice = product.UnitPrice, Quantity = 1}
+        );
         
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("The product {0} has been added", product.Name);
         Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    internal void RemoveAtIndex(int v)
+    {
+        OrderLines.RemoveAt(v);
     }
 }
