1761a6e2ae391d695fb700ea4b3c58d1edb9d1d3
qasas145 <mohamed.sayed14527@yahoo.com>
2024-10-25 23:19:23 +0300

    Methods related to memento added

--- DesignPatterns/CommandBasics/Models/OrderC.cs
+++ DesignPatterns/CommandBasics/Models/OrderC.cs
@@ -14,4 +14,13 @@ public class OrderC : Order{
     {
         OrderLines.RemoveAt(v);
     }
+
+    internal void TakeMemento() {
+        var memento = new Memento(){OrderLines = OrderLines.Select(oL=>oL).ToList()};
+        CareTaker.Instance.AddMemento(memento);
+    }
+    internal void ReturnToMemento(int id) {
+        var memento = CareTaker.Instance.GetMemento(id);
+        OrderLines = memento.OrderLines;
+    }
 }
