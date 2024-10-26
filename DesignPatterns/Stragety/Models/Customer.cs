49eaf1516a4f2c93bee51e51f6b8cb3a59003f1e
qasas145 <mohamed.sayed14527@yahoo.com>
2024-10-26 08:17:47 +0300

    adding some attributes to fit the salaryCalculation method

--- DesignPatterns/Stragety/Models/Customer.cs
+++ DesignPatterns/Stragety/Models/Customer.cs
@@ -1,4 +1,8 @@
 public class Customer {
     public int Id{get;set;}
     public States Status{get;set;}
+    public string Name { get; set; }
+    public string JobTitle { get; set; }
+    public double BaseSalary { get; set; }
+    public int YearsOfExperience { get; set; }
 }
