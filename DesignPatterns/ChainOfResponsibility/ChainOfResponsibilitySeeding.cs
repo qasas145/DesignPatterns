fa584a23ce9f78cf54cddd7de64f42f00b726d6e
qasas145 <mohamed.sayed14527@yahoo.com>
2024-10-26 15:22:29 +0300

    Adding validators for dates

--- DesignPatterns/ChainOfResponsibility/ChainOfResponsibilitySeeding.cs
+++ DesignPatterns/ChainOfResponsibility/ChainOfResponsibilitySeeding.cs
@@ -8,19 +8,21 @@ public class ChainOfResponsibilitySeeding {
         var request = new VacactionRequest(){
             Employee = employee,
             StartDate = DateTime.Now,
-            EndDate = DateTime.Now.AddDays(4),
+            EndDate = DateTime.Now.AddDays(-2),
         };
 
+        var validateDates =new ValidateDates();
         var CTOVacationHandler = new CTOVacationHandler();
         var CEOVacationHandler = new CEOVacationHandler();
         var teamLeaderVacationHandler = new TeamLeaderVacationHandler();
         var technicalVacationHandler = new TechnicalManagerVacationHanlder();
         
+        validateDates.SetNext(teamLeaderVacationHandler);
         teamLeaderVacationHandler.SetNext(technicalVacationHandler);
         technicalVacationHandler.SetNext(CTOVacationHandler);
         CTOVacationHandler.SetNext(CEOVacationHandler);
 
-        teamLeaderVacationHandler.Process(request);
+        validateDates.Process(request);
 
     }
 }
